d4735aea01cfe506f03af61b3b51eb19ae156e63
Nickpizzablock <ndtorres@ucsd.edu>
2023-06-08 11:36:53 -0700

    removed comments bwamin

--- bwamin.py
+++ bwamin.py
@@ -47,7 +47,7 @@ if optionsList.count(True) != 1:
 # sw does not have to be indexed
 optionsList = [args.index, args.sw]
 if optionsList.count(True) == 2:
-    print('ERROR: sw does not require to be indexed: don\'t use index', file=sys.stderr)
+    print('ERROR: cannot index with sw, use --mem instead', file=sys.stderr)
     sys.exit(1)
 
 # File path checking
@@ -104,7 +104,7 @@ if '.sam' not in args.out:
 try:
     zenith = open(args.out,'w')
 except IOError:
-    print('Output file does not want to be opened. Check chmod', file=sys.stderr)
+    print('Output file does\'t want to be opened. Check chmod', file=sys.stderr)
 
 # Making the header on zenith
 # https://samtools.github.io/hts-specs/SAMv1.pdf
@@ -120,26 +120,24 @@ zenith.write(sambuild.pg('bwamin', 'bwamin', '0.001-alpha', command))
 # For bwt
 if args.bwt:
     # Read corresponding index for bwt
-    
     faDict = align.alignGenome(faFile + '.myindex')
     for i in fqOut:
         foundMatch = False
         for j in faDict:
             leftpos = []
-            # leftpos = bwt.find(faDict[j], fqOut[i][0])
             leftpos = bwt.find(faDict[j], fqOut[i][0])
-
+           
             # Perfect match
             quality = 60
             if leftpos != None and len(leftpos) == 1:
                 flag = 0
                 pos = leftpos[0]
-                # print('found at least 1 exact match')
-                #exact matching only
-                #flag = 0 because not checking reverse
-                #i think leftpos is one off
+                
+                # Exact matching only
+                # flag = 0 because not checking reverse
+                # Pos by 0-index
                 foundMatch = True
-                zenith.write(sambuild.readToString(i.split(' ', 1)[0].strip(), flag, j, pos, quality, str(len(fqOut[i][0])) + 'M', "rnext", "pnext", "tlen", fqOut[i][0], fqOut[i][1])) # note: you need to put \n
+                zenith.write(sambuild.readToString(i.split(' ', 1)[0].strip(), flag, j, pos, quality, str(len(fqOut[i][0])) + 'M', "rnext", "pnext", "tlen", fqOut[i][0], fqOut[i][1]))
                 break
             else:
                 #try reverse string
@@ -149,66 +147,34 @@ if args.bwt:
                 if leftpos != None and len(leftpos) == 1:
                     flag = 16               # bitwise code for reverse
                     pos = leftpos[0] + 1    # +1 to be 1 index
-                    # print('found at least 1 exact match reversed')
                     foundMatch = True
-                    zenith.write(sambuild.readToString(i.split(' ', 1)[0].strip(), flag, j, pos, quality, str(len(fqOut[i][0])) + 'M', "rnext", "pnext", "tlen", fqOut[i][0], fqOut[i][1])) # note: you need to put \n
+                    zenith.write(sambuild.readToString(i.split(' ', 1)[0].strip(), flag, j, pos, quality, str(len(fqOut[i][0])) + 'M', "rnext", "pnext", "tlen", fqOut[i][0], fqOut[i][1]))
                     break
-                
-                # Could not find perfect match
-                    
+        # Could not find perfect match          
         if not foundMatch:
             flag = 4
             pos = 0
-            zenith.write(sambuild.readToString(i.split(' ', 1)[0].strip(), flag, '*', pos, 0, '*', "*", 0, 0, fqOut[i][0], fqOut[i][1])) # note: you need to put \n
+            zenith.write(sambuild.readToString(i.split(' ', 1)[0].strip(), flag, '*', pos, 0, '*', "*", 0, 0, fqOut[i][0], fqOut[i][1]))
     zenith.close()
     exit()
 
 # SW 
-
 for i in fqOut:
-    # genomeScores = []
     bestAlignments = {}
-
     for j in faOut.keys():
-        # genomeScores.append(align.alignThem(j, i))
-        # if align.alignThem((j, faOut[j]), (i, fqOut[i])):
-        #     print("there is a match")
         bestAlignments[j] = align.nwAlgo(j, faOut[j], i, fqOut[i], match, mismatch, indel, gapPenalty)
 
-        # Not using quality score but matching score 
+        # Not using quality score but matching score
+        # Score max 60 min 0
         if bestAlignments[j][0] < 0:
             bestAlignments[j][0] = 0
         if bestAlignments[j][0] > 60:
             bestAlignments[j][0] = 60
         # Note: flag = 0 since only detects reads
-        # quality = bestAlignments[j][0]
+    # Find highest score alignment
     key, highest = align.maxAlignmentDict(bestAlignments)
     flag = 0
     quality = highest[0]
 
-    # zenith.write(sambuild.readToString(i, flag, j, bestAlignments[j][2], quality, bestAlignments[j][3], '*', 0, 0, fqOut[i][0], fqOut[i][1])) # note: you need to put \n
-        
-    zenith.write(sambuild.readToString(i.strip(), flag, key, highest[2], quality, highest[3], '*', 0, 0, fqOut[i][0], fqOut[i][1])) # note: you need to put \n
-        
-        # zenith.write('hi\thi')
-
-        # exit()
-    # bestScore = max(genomeScores)
-    # samOut.append(bestScore)
-# align.nwAlgo()
-# Writing the contents out
-# with open("output.sam", "w") as writer:
-#     for i in samOut:
-#         writer.write(i)
-# print(optionsList.count(True))
-# parser.add_argument("index", type=int, required=True, help="match val")
-# parser.add_argument("mem", type=int, required=True, help="mismatch val")
+    zenith.write(sambuild.readToString(i.strip(), flag, key, highest[2], quality, highest[3], '*', 0, 0, fqOut[i][0], fqOut[i][1]))
 zenith.close()
-# zenith = open('zenith.txt','r')
-# print('----------ZenithFile-----------')
-# print(zenith.read())
-# print('----------ZenithFile-----------')
-
-
-# print(bestAlignments)
-# print('hellowaorld')
